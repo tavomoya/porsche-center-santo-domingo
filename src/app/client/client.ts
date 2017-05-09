0cf25272557e0721e0478973b71dbf66245a2f68
Gael Leonel Abreu <Gael_Abreu@hotmail.com>
2017-05-09 04:03:49 -0400

    Added missing components

---
+++ src/app/client/client.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ModalDirective } from 'ng2-bootstrap/modal/modal.component';
+
+@Component({
+	templateUrl: 'client.html'
+})
+export class ClientComponent {
+
+	constructor() { }
+
+}
